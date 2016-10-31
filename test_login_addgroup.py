5b94d6df0c06d62cb9ea15dac0f0e8b838bb3816
Jensen QA <mystockholmdream@gmail.com>
2016-10-31 12:17:20 +0300

    step 3
    рефакторинг кода
    удалены комментарии
    удалены ненужные элементы

--- test_login_addgroup.py
+++ test_login_addgroup.py
@@ -14,14 +14,22 @@ class test_login_addgroup(unittest.TestCase):
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
     
+
     def test_test_login_addgroup(self):
         wd = self.wd
-        # open home page
+        self.open_home_page(wd)
+        self.login(wd)
+        self.open_group_page(wd)
+        self.create_group(wd)
+        self.return_to_group_page(wd)
+        self.logout(wd)
+
+
+    def open_home_page(self, wd):
         wd.get("http://localhost/addressbook/")
-        # login
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").send_keys("\\undefined")
-        wd.find_element_by_css_selector("body").click()
+
+
+    def login(self, wd):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
@@ -29,32 +37,35 @@ class test_login_addgroup(unittest.TestCase):
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-        # open groups page
+
+
+    def open_group_page(self, wd):
         wd.find_element_by_link_text("groups").click()
+
+
+    def create_group(self, wd):
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
         wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys("newgroup")
         wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
         wd.find_element_by_name("group_header").send_keys("yey")
         wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys("yey")
         # submit group creation
         wd.find_element_by_name("submit").click()
-        # return to groups page
-        wd.find_element_by_link_text("group page").click()
-        # logout
-        wd.find_element_by_link_text("Logout").click()
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").send_keys("\\undefined")
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").send_keys("\\undefined")
 
-    
+
+    def return_to_group_page(self, wd):
+        wd.find_element_by_link_text("group page").click()
+
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+
+
     def tearDown(self):
         self.wd.quit()
 
