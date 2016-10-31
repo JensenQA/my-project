481fb23c599187f7cd565e529e716463b55f28e4
Jensen QA <mystockholmdream@gmail.com>
2016-10-31 14:53:53 +0300

    step 8
    Выделен класс для управления сессиями

--- tests/test_login_addgroup.py
+++ tests/test_login_addgroup.py
@@ -14,12 +14,12 @@ def app(request):
 
 def test_login_add_group(app):
         app.open_home_page()
-        app.login(username="admin", password="secret")
+        app.session.login(username="admin", password="secret")
         app.create_group(Group(name="new_group", header="description", footer="description_2"))
-        app.logout()
+        app.session.logout()
 
 def test_login_add_group_empty(app):
         app.open_home_page()
-        app.login(username="admin", password="secret")
+        app.session.login(username="admin", password="secret")
         app.create_group(Group(name="", header="", footer=""))
-        app.logout()
+        app.session.logout()
